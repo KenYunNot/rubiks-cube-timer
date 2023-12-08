94adcfd3ddf4e01329d210603926408ba3f8e061
Ken Yun <yunhaen@gmail.com>
2023-12-08 13:54:26 -0500

    Moved states and dispatch to parent page, passed as context props

--- app/ui/timer.tsx
+++ app/ui/timer.tsx
@@ -1,105 +1,111 @@
 "use client";
 
-import { useState, useEffect, useRef, MutableRefObject } from "react";
-import { buildDisplayElement, startInspectionTimer, startSolveTimer } from "@/app/lib/utils"
+import { useState, useEffect, useRef, useContext } from "react";
+import { 
+  startInspectionTimer, 
+  startSolveTimer,
+  buildTimerElement,
+  addSolve,
+} from "@/app/lib/utils"
+import {
+  BLACK,
+  RED,
+  YELLOW,
+  GREEN,
+  IDLE,
+  STARTING,
+  INSPECTION,
+  WAITING,
+  READY,
+  SOLVING,
+  FINISHED,
+  INTERRUPTED,
+} from '@/app/lib/definitions';
+import { StatusContext } from "@/app/context/statusContext";
+import { ScramblesContext, ScramblePointerContext } from "@/app/context/scrambleContext";
+import { SolvesContext } from "@/app/context/solvesContext";
 
-
-const BLACK = "text-black";
-const RED = "text-red-500";
-const YELLOW = "text-yellow-400";
-const GREEN = "text-green-500";
-
-const IDLE = "IDLE";
-const STARTING = "STARTING"
-const INSPECTION = "INSPECTION";
-const READY = "READY";
-const SOLVING = "SOLVING";
-const FINISHED = "FINISHED";
 
 export default function Timer() {
-  const [display, setDisplay] = useState<string | string[]>(["0", "00"]); // string means inspection state, string[] means timer state
-  const [displayColor, setDisplayColor] = useState<string>(BLACK); // Current color of the display, uses constants defined as the tailwind color classes
+  let { status } = useContext(StatusContext); // Fetch value of status from Context Provider
+  let { scrambles } = useContext(ScramblesContext); // Fetch scrambles from Scramble context provider
+  let { scramblePointer } = useContext(ScramblePointerContext); // Fetch scramblePointer from ScramblePointer context provider
+  let { setSolves } = useContext(SolvesContext) // Fetch solves and dispatch from Solves context provider
 
-  const status = useRef<string>(IDLE); // Current status of the timer, uses constants defined as their string representations
-  const readyTime = useRef<number>(300); // How long the user must hold down the space bar in milliseconds to start the solve 
+  const [timer, setTimer] = useState<string | number[]>([0,0,0,0,0]); // Currently displayed on the timer
+  const [timerColor, setTimerColor] = useState<string>(BLACK); // Current color of the timer
 
-  const setStatus = (newStatus: string) => status.current = newStatus; // Mutate function for status ref object
+  const inspectionInterval = useRef<NodeJS.Timeout | undefined>(undefined); // Interval ID for inspection interval (fires every second)
+  const solveInterval = useRef<NodeJS.Timeout | undefined>(undefined); // Interval ID for solve interval (fires every centisecond)
+  
+  const DNF = useRef<boolean>(false);
+  const plusTwo = useRef<boolean>(false);
 
-  const inspectionInterval = useRef<NodeJS.Timeout | undefined>(undefined); // Interval used for the inspection
-  const readyTimeInterval = useRef<NodeJS.Timeout | undefined>(undefined); // Interval used to verify if the user held down the space bar for the minimum time
-  const solveInterval = useRef<NodeJS.Timeout | undefined>(undefined); // Interval used for the solve
-
+  /* On status change useEffect */
   useEffect(() => {
-    // 'keydown' event listener
-    addEventListener("keydown", (event) => {
-      // ' ' (space bar) events
-      if (event.key === ' ') {
-        switch (status.current) {
-          case IDLE:
-            setDisplayColor(GREEN);
-            setStatus(STARTING);
-            break;
-          case STARTING:
-            break;
-          case INSPECTION:
-            setDisplayColor(YELLOW);
-            let intervalId = setInterval(() => {
-              clearInterval(intervalId);
-              setDisplayColor(GREEN);
-              setStatus(READY);
-            }, readyTime.current);
-            readyTimeInterval.current = intervalId;
-            break;
-          case READY:
-            break;
-          case SOLVING:
-            clearInterval(solveInterval.current);
-            setDisplayColor(RED);
-            setStatus(FINISHED);
-            break;
-          case FINISHED:
-            break;
-        }
-      }
-    });
+    switch (status) {
+      case IDLE:
+        setTimerColor(BLACK);
+        cleanTimer();
+        break;
+      case STARTING:
+        setTimerColor(GREEN);
+        break;
+      case INSPECTION:
+        if (!inspectionInterval.current) inspectionInterval.current = startInspectionTimer(setTimer, DNF, plusTwo);
+        setTimerColor(RED);
+        break;
+      case WAITING:
+        setTimerColor(YELLOW);
+        break;
+      case READY:
+        setTimerColor(GREEN);
+        break;
+      case SOLVING:
+        clearInterval(inspectionInterval.current);
+        if (!solveInterval.current) solveInterval.current = startSolveTimer(setTimer);
+        setTimerColor(BLACK);
+        break;
+      case FINISHED:
+        let solveInfo = {
+          timer,
+          scramble: scrambles[scramblePointer],
+          DNF: DNF.current,
+          plusTwo: plusTwo.current,
+        };
+        setSolves(addSolve(solveInfo)); // Add solve to localStorage and manually on client
+        clearInterval(solveInterval.current);
+        setTimerColor(RED);
+        break;
+      case INTERRUPTED:
+        resetDisplay();
+        cleanTimer();
+        break;
+      default:
+        console.log(`Invalid status ${status}.`);
+        break;
+    }
+  }, [status]);
 
-    // 'keyup' event listener
-    addEventListener("keyup", (event) => {
-      // ' ' (space bar) events
-      if (event.key === ' ') {
-        switch (status.current) {
-          case IDLE:
-            break;
-          case STARTING:
-            setDisplayColor(RED);
-            let inspectionIntervalId = startInspectionTimer(setDisplay, status.current);
-            inspectionInterval.current = inspectionIntervalId;
-            setStatus(INSPECTION);
-            break;
-          case INSPECTION:
-            clearInterval(readyTimeInterval.current);
-            setDisplayColor(RED);
-            break;
-          case READY:
-            clearInterval(inspectionInterval.current);
-            setDisplayColor(BLACK);
-            let solveIntervalId = startSolveTimer(setDisplay);
-            solveInterval.current = solveIntervalId;
-            setStatus(SOLVING);
-          case SOLVING:
-            break;
-          case FINISHED:
-            setDisplayColor(BLACK);
-            setStatus(IDLE);
-            break;
-        }
-      }
-    });
-  }, [])
+  
+  function resetDisplay() {
+    setTimer([0,0,0,0,0]);
+  }
+
+  function cleanTimer() {
+    setTimerColor(BLACK);
+    clearInterval(inspectionInterval.current);
+    clearInterval(solveInterval.current);
+    inspectionInterval.current = undefined;
+    solveInterval.current = undefined;
+    DNF.current = false;
+    plusTwo.current = false;
+  }
+
 
   return (
-    <div id="timer-display" className="flex justify-center">
-      <p className={`text-9xl ${displayColor}`}>{buildDisplayElement(display)}</p>
+    <div id="timer-div" className="absolute top-1/4">
+      <p id="timer" className={timerColor}>{buildTimerElement(timer)}</p>
     </div>
   );
 }
