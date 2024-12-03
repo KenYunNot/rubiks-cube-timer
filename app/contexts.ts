cd8901e2b357d75e94973f253e483f3c11726e1d
Ken Yun <yunhaen@gmail.com>
2024-12-02 21:17:56 -0500

    refactor: moved contexts to lib

--- app/contexts.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { Settings, Status } from "@/app/lib/definitions";
-
-export const SettingsContext = createContext({} as Settings);
-export const StatusContext = createContext({} as Status);
