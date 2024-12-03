d9c861bcd9596517c4f089296e5362c78bae04b9
Ken Yun <yunhaen@gmail.com>
2024-12-02 21:59:29 -0500

    fix: cleaned up code and moved relevant states and hooks to timer component

--- app/page.tsx
+++ app/page.tsx
@@ -3,104 +3,18 @@
 import { useState, useEffect, useRef } from "react";
 import { Status, Settings } from "@/app/lib/definitions";
 import Timer from "@/app/ui/timer";
-import { StatusContext, SettingsContext } from "@/app/lib/contexts";
 
 export default function Home() {
   const [scramble, setScramble] = useState<string>();
-  const [status, setStatus] = useState<Status>("idle");
   const [settings, setSettings] = useState<Settings>({
     readyTime: 300,
     scrambleSize: 15,
     useInspection: true,
   });
 
-  const interval = useRef<NodeJS.Timeout>();
-  const intervalFn = () => {
-    setStatus("ready");
-    clearInterval(interval.current);
-  }
-
-  function handleKeyDown(e: any) {
-    // Handle 'Escape' events separately
-    if (e.key === 'Escape') {
-      setStatus("idle");
-    }
-    // Ignore events that are not space (except Escape) and those that repeat
-    if (e.key !== ' ' || e.repeat) return;
-    if (!settings.useInspection) {
-      switch (status) {
-        case "idle":
-          setStatus("waiting");
-          interval.current = setInterval(intervalFn, settings.readyTime);
-          break;
-        case "solving":
-          setStatus("finished");
-          break;
-      }
-    } else {
-      switch (status) {
-        case "idle":
-          setStatus("starting");
-          break;
-        case "inspection":
-          setStatus("waiting");
-          interval.current = setInterval(intervalFn, settings.readyTime);
-          break;
-        case "solving":
-          setStatus("finished");
-          break;
-      }
-    }
-  }
-
-  function handleKeyUp(e: any) {
-    // Ignore events that are not space and those that repeat
-    if (e.key !== ' ' || e.repeat) return;
-    if (!settings.useInspection) {
-      switch (status) {
-        case "waiting":
-          setStatus("idle");
-          clearInterval(interval.current);
-          break;
-        case "ready":
-          setStatus("solving");
-          break;
-        case "finished":
-          setStatus("idle");
-          break;
-      }
-    } else {
-      switch (status) {
-        case "starting":
-          setStatus("inspection");
-          break;
-        case "waiting":
-          setStatus("inspection");
-          clearInterval(interval.current);
-          break;
-        case "ready":
-          setStatus("solving");
-          break;
-        case "finished":
-          setStatus("idle");
-          break;
-      }
-    }
-  }
-
-  useEffect(() => {
-    addEventListener("keydown", handleKeyDown);
-    addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      removeEventListener("keydown", handleKeyDown);
-      removeEventListener("keyup", handleKeyUp)
-    }
-  }, [status])
-
   return (
     <div className="w-full h-full">
-      <Timer status={status} settings={settings} />
+      <Timer settings={settings} />
     </div>
   )
 }
--- app/ui/timer.tsx
+++ app/ui/timer.tsx
@@ -1,61 +1,163 @@
 "use client";
 
 import React from "react";
-import { generateDisplay } from "@/app/lib/utils";
-import { StatusContext, SettingsContext } from "@/app/lib/contexts";
 import clsx from "clsx";
 import type { Status, Settings } from "@/app/lib/definitions";
 
 type TimerProps = {
-  status: Status;
   settings: Settings;
 }
 
+
+const generateDisplay = (startTime: number, endTime: number) => {
+  const DAY_MS = 86400000;
+  const HOUR_MS = 3600000;
+  const MINUTE_MS = 60000;
+  const SECOND_MS = 1000;
+  const CENTISECOND_MS = 10;
+
+  let parts = [];
+  let diff = endTime - startTime;
+  if (diff > DAY_MS) {
+    parts.push(Math.trunc(diff / DAY_MS));
+  }
+  if (diff > HOUR_MS) {
+    parts.push(Math.trunc((diff % DAY_MS) / HOUR_MS));
+  }
+  if (diff > MINUTE_MS) {
+    parts.push(Math.trunc((diff % HOUR_MS) / MINUTE_MS));
+  }
+  parts.push(Math.trunc((diff % MINUTE_MS) / SECOND_MS));
+  parts.push(Math.trunc((diff % SECOND_MS) / CENTISECOND_MS));
+
+  let display = [];
+  for (let i = 0; i < parts.length; i++) {
+    if (i === 0) {
+      display.push(String(parts[i]));
+    } else {
+      display.push(String(parts[i]).padStart(2, '0'));
+    }
+  }
+  return display;
+}
+
 export default function Timer({
-  status,
   settings,
 } : TimerProps) {
   const [display, setDisplay] = React.useState<string[]>(["0", "00"]);
+  const [status, setStatus] = React.useState<Status>("idle");
   const inspectionInterval = React.useRef<NodeJS.Timeout>();
   const solvingInterval = React.useRef<NodeJS.Timeout>();
 
-  React.useEffect(() => {
-    /* idle status */
-    if (status === "idle") {
-      clearInterval(inspectionInterval.current);
-      clearInterval(solvingInterval.current);
+  const waitingInterval = React.useRef<NodeJS.Timeout>();
+  const intervalFn = () => {
+    setStatus("ready");
+    clearInterval(waitingInterval.current);
+  }
+
+  const handleKeyDown = (e: any) => {
+    // Handle 'Escape' events separately
+    if (e.key === 'Escape') {
+      setStatus("idle");
     }
-    /* inspection status */
-    if (status === "inspection") {
-      // If the inspection timer is already started, don't create another one
-      if (inspectionInterval.current !== undefined) return;
-      let time = 15;
-      setDisplay([String(time)]);
-      inspectionInterval.current = setInterval(() => {
-        time -= 1;
-        if (time <= -2) {
-          setDisplay(["DNF"]);
-        } else if (time <= 0) {
-          setDisplay(["+2"]);
-        } else {
+    // Ignore events that are not space (except Escape) and those that repeat
+    if (e.key !== ' ' || e.repeat) return;
+    if (settings.useInspection) {
+      switch (status) {
+        case "idle":
+          setStatus("starting");
+          break;
+        case "inspection":
+          setStatus("waiting");
+          waitingInterval.current = setInterval(intervalFn, settings.readyTime);
+          break;
+        case "solving":
+          setStatus("finished");
+          break;
+      }
+    } else {
+      switch (status) {
+        case "idle":
+          setStatus("waiting");
+          waitingInterval.current = setInterval(intervalFn, settings.readyTime);
+          break;
+        case "solving":
+          setStatus("finished");
+          break;
+      }
+    }
+  }
+
+  const handleKeyUp = (e: any) => {
+    // Ignore events that are not space and those that repeat
+    if (e.key !== ' ' || e.repeat) return;
+    if (settings.useInspection) {
+      switch (status) {
+        case "starting":
+          setStatus("inspection");
+          let time = 15;
           setDisplay([String(time)]);
-        }
-      }, 1000);
+          inspectionInterval.current = setInterval(() => {
+            time -= 1;
+            if (time <= -2) {
+              setDisplay(["DNF"]);
+            } else if (time <= 0) {
+              setDisplay(["+2"]);
+            } else {
+              setDisplay([String(time)]);
+            }
+          }, 1000);
+          break;
+        case "waiting":
+          setStatus("inspection");
+          clearInterval(waitingInterval.current);
+          break;
+        case "ready":
+          setStatus("solving");
+          clearInterval(inspectionInterval.current);
+          const startTime = Date.now();
+          solvingInterval.current = setInterval(() => {
+            const currentTime = Date.now();
+            const newDisplay = generateDisplay(startTime, currentTime);
+            setDisplay(newDisplay);
+          }, 10);
+          break;
+        case "finished":
+          setStatus("idle");
+          clearInterval(solvingInterval.current);
+          break;
+      }
+    } else {
+      switch (status) {
+        case "waiting":
+          setStatus("idle");
+          clearInterval(waitingInterval.current);
+          break;
+        case "ready":
+          setStatus("solving");
+          clearInterval(inspectionInterval.current);
+          const startTime = Date.now();
+          solvingInterval.current = setInterval(() => {
+            const currentTime = Date.now();
+            const newDisplay = generateDisplay(startTime, currentTime);
+            setDisplay(newDisplay);
+          }, 10);
+          break;
+        case "finished":
+          setStatus("idle");
+          clearInterval(solvingInterval.current);
+          break;
+      }
     }
-    /* solving status */
-    if (status === "solving") {
-      clearInterval(inspectionInterval.current);
-      const startTime = Date.now();
-      solvingInterval.current = setInterval(() => {
-        const currentTime = Date.now();
-        const newDisplay = generateDisplay(startTime, currentTime);
-        setDisplay(newDisplay);
-      }, 10);
-    }
-    /* finished status */
-    if (status === "finished") {
-      clearInterval(solvingInterval.current);
-      inspectionInterval.current = undefined;
+  }
+
+  React.useEffect(() => {
+    addEventListener("keydown", handleKeyDown);
+    addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      removeEventListener("keydown", handleKeyDown);
+      removeEventListener("keyup", handleKeyUp)
     }
   }, [status])
 
