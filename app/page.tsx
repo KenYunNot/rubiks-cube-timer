e6228584790dc920bc43d790cc1d6c633e4f8ba0
Ken Yun <yunhaen@gmail.com>
2024-12-07 14:25:08 -0500

    fix: apply flex to main container

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { useState } from "react";
+import Scramble from "@/app/ui/scramble";
 import Timer from "@/app/ui/timer";
 
 export default function Home() {
   const [scramble, setScramble] = useState<string>();
 
   return (
-    <div className="w-full h-full">
+    <div className="flex flex-col w-full h-full">
+      <Scramble />
       <Timer />
     </div>
   )
